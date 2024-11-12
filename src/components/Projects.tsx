6ed93a7a415976010af890b322b39a952f21bda9
FaizaSiddiqu <160780189+FaizaSiddiqu@users.noreply.github.com>
2024-11-12 22:40:38 +0500

    updated code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,45 +6,45 @@ const data = [
   {
     id: 0 ,
     tittle: "ToDo-List",
-    desc: "A React and Typrscript based App for managing and organizing your tasks efficiently.",
-    img: "/project_00.jpg",
-    tags: ["React", "Node" , "CSS" , "Typescript"]
+    desc: "An HtML and Javascript based App for managing and organizing your tasks efficiently.",
+    img: "/todo.jpg",
+    tags: ["HTML", "Node" , "CSS" , "Javascript"]
   },
   {
     id: 1 ,
     tittle: "Countown-Timer",
     desc: "A Next.js and Typescript powered website to track time with an interactive countown feature",
-    img: "/project_00.jpg",
+    img: "/countdown1.jpg",
     tags: ["React", "Node" , "CSS" , "Typescript"]
   },
   {
     id: 2 ,
     tittle: "Weather Widget",
     desc: "A Next.js and Typrscript based App for fetching and displaying real-time weather data.",
-    img: "/project_00.jpg",
+    img: "/whetherwidget.png",
     tags: ["React", "Node" , "CSS" , "Typescript"]
   },
   {
     id: 3 ,
-    tittle: "Currency Convertor Project",
-    desc: "A Simple HTML and Typescript powered tool for converting ciurrencies with real-time rates.",
-    img: "/project_00.jpg",
-    tags: ["React", "Node" , "CSS" , "Typescript"]
+    tittle: "Online Coffee Shop",
+    desc: "An online coffee shop built with Next.js and Tailwind CSS, Enjoy a responsive and interactive interface designed for coffee enthusiasts.",
+    img: "/coffee.jpg",
+    tags: ["Next.js", "Node" , "Tailwind.CSS" , "Typescript"]
   },
   {
     id: 4,
     tittle: "Static Interactive Resume",
     desc: "A Typrscript based interactive Resume built with HTML and CSS ,allowing users to showcase their skills dynamically.",
-    img: "/project_00.jpg",
+    img: "/resume1.png",
     tags: ["React", "Node" , "CSS" , "Typescript"]
     
   },
   {
     id: 0 ,
     tittle: "Simple Calculator Projects",
-    desc: "A React and Typrscript based App for managing and organizing your tasks efficiently.",
-    img: "/project_00.jpg",
-    tags: ["React", "Node" , "CSS" , "Typescript"]
+    desc: "A simple calculator built with HTML, CSS, and TypeScript, designed for performing basic calculations efficiently and with a clean interface.",
+    img: "/cal-1.png",
+    tags: ["HTML", "Node" , "CSS" , "Typescript"]
   },
   
   
